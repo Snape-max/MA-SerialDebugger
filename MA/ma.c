7555e682d7e394aa8e0edfa045f7b8d4b77be36f
Luke <69849470+Snape-max@users.noreply.github.com>
2023-08-10 20:41:42 +0800

    Add files via upload
    MCU库文件

---
+++ MA/ma.c
@@ -0,0 +1,19 @@
+#include "ma.h"
+
+
+void MA_SendData(uint_8 *data, int8 datalen){
+    MA_Send_Bytes_CallBack(Frame_B_1);
+    MA_Send_Bytes_CallBack(Frame_B_2);
+    for(int i=0;i<datalen;i++){
+        MA_Send_Bytes_CallBack(data[i])
+    }
+    MA_Send_Bytes_CallBack(Frame_E_1);
+    MA_Send_Bytes_CallBack(Frame_E_2);
+}
+
+
+void MA_Send_Bytes_CallBack(uint_8 data){
+
+    //回调函数,更换为自己的串口发送函数
+
+}
---
+++ MA/ma.h
@@ -0,0 +1,14 @@
+#ifndef __MA_H
+#define __MA_H
+
+
+#define Frame_B_1 0xfa
+#define Frame_B_2 0xac
+#define Frame_E_1 0xcc
+#define Frame_E_2 0xff
+
+void MA_SendData(uint_8 *data, int8 datalen);
+
+
+
+#endif
